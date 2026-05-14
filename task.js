58a040ada0768c36c474a90b184c4d6933e92d43
softCaim <vasil.berozashvili.1@iliauni.edu.ge>
2026-05-14 13:20:24 +0400

    First commit

---
+++ task.js
@@ -0,0 +1,6 @@
+console.log("Name is : Miranda \n rame");
+console.log("Surname is : Arabuli");
+console.log("Age : 100");
+console.warn("Email : Test@gmail.com");
+console.warn("Phone :  123456");
+console.log("Phone :  555-555-555");
