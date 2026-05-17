8d015f5a23de05c29bf5cfbf592b2e1c089360bc
softCaim <vasil.berozashvili.1@iliauni.edu.ge>
2026-05-17 13:58:48 +0400

    dabadebis tarigis gamotvla

--- homework_2/script.js
+++ homework_2/script.js
@@ -1,10 +1,4 @@
-let greeting = "Hello!";
-
-let first_number = 10;
-let second_number = 20;
-
-const third_number = 30;
-
-console.log(first_number + second_number);
-greeting = "გამარჯობა";
-console.log(greeting);
+const my_birthday = 2004;
+let current_year = 2026;
+let my_age = current_year - my_birthday;
+console.log(my_age);
