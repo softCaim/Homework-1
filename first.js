b6f0681de89497fb04156fddaa55b24721f91b17
softCaim <vasil.berozashvili.1@iliauni.edu.ge>
2026-05-17 12:43:00 +0400

    axali folderebi

---
+++ first.js
@@ -0,0 +1,4 @@
+console.log("hy");
+console.log(2 + 2);
+console.warn("vax");
+console.log();
--- homework_1/task.js
+++ homework_1/task.js
@@ -4,3 +4,5 @@ console.log("Age : 100");
 console.warn("Email : Test@gmail.com");
 console.warn("Phone :  123456");
 console.log("Phone :  555-555-555");
+
+let greeting = "Hello!";
