34b3b7d060c2169342be49ae619f472cfbb504d0
softCaim <vasil.berozashvili.1@iliauni.edu.ge>
2026-05-17 12:27:58 +0400

    homeworks_2

---
+++ script.js
@@ -0,0 +1,11 @@
+let greeting = "Hello!";
+
+let first_number = 10;
+let second_number = 20;
+
+const third_number = 30;
+
+third_number = 40;
+console.log(first_number + second_number);
+greeting = "გამარჯობა";
+console.log(greeting);
