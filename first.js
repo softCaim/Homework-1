1357064cb592ea6325af608e1f73082946f4c671
softCaim <vasil.berozashvili.1@iliauni.edu.ge>
2026-05-17 14:00:54 +0400

    leqcia chaviwere

--- first.js
+++ first.js
@@ -2,3 +2,13 @@ console.log("hy");
 console.log(2 + 2);
 console.warn("vax");
 console.log();
+let greeting = "Hello!";
+
+let first_number = 10;
+let second_number = 20;
+
+const third_number = 30;
+
+console.log(first_number + second_number);
+greeting = "გამარჯობა";
+console.log(greeting);
