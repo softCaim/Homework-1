b9df57b4dcb2c65b94165adcb85049936e88d3ed
softCaim <vasil.berozashvili.1@iliauni.edu.ge>
2026-05-30 11:37:03 +0400

    failebis da directoriebis dasaxelebebis cvlileba

--- switch.js
+++ switch.js
@@ -9,6 +9,34 @@ switch (true) {
     break;
   case age > 50:
     console.log("ასაკი 50-ზე მეტია");
+    break; // break ვუწერთ იმიტომ რომ კონსოლში შემოწმებული მნიშვნელობის მერე არ გაგრძელდეს switch-ის გაშვება
+  default:
+    console.log("არასწორი მნიშვნელობა");
+}
+
+let day = 1;
+
+switch (day) {
+  case 0:
+    console.log("ორშაბათი");
+    break;
+  case 1:
+    console.log("სამშაბათი");
+    break;
+  case 2:
+    console.log("ოთხშაბათი");
+    break;
+  case 3:
+    console.log("ხუთშაბათი");
+    break;
+  case 4:
+    console.log("პარასკევი");
+    break;
+  case 5:
+    console.log("შაბათი");
+    break;
+  case 6:
+    console.log("კვირა");
     break;
   default:
     console.log("არასწორი მნიშვნელობა");
